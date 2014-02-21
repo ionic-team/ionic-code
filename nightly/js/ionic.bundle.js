8b35ddde773f8c3d6afc2d4cb5e0461d0619f89b
Ionotron <hi@ionicframework.com>
2014-02-21 15:53:28 +0000

    release: 0.9.26-alpha-889 (nightly)

--- nightly/js/ionic.bundle.js
+++ nightly/js/ionic.bundle.js
@@ -8,7 +8,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v0.9.26-alpha-886
+ * Ionic, v0.9.26-alpha-889
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -23,7 +23,7 @@
 window.ionic = {
   controllers: {},
   views: {},
-  version: '0.9.26-alpha-886'
+  version: '0.9.26-alpha-889'
 };
 ;
 (function(ionic) {
@@ -31625,7 +31625,7 @@ angular.module('ui.router.compat')
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v0.9.26-alpha-886
+ * Ionic, v0.9.26-alpha-889
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -34265,9 +34265,9 @@ angular.module('ionic.ui.tabs', ['ionic.service.view', 'ngSanitize'])
     template:
       '<a ng-class="{active:active, \'has-badge\':badge}" ng-click="selectTab()" class="tab-item">' +
         '<span class="badge {{badgeStyle}}" ng-if="badge">{{badge}}</span>' +
-        '<span class="icon {{icon}}" ng-if="icon"></span>' +
-        '<span class="{{iconOn}}" ng-if="active"></span>' +
-        '<span class="{{iconOff}}" ng-if="!active"></span>' +
+        '<i class="icon {{icon}}" ng-if="icon"></i>' +
+        '<i class="icon {{iconOn}}" ng-if="active"></i>' +
+        '<i class="icon {{iconOff}}" ng-if="!active"></i>' +
         '<span ng-bind-html="iconTitle"></span>' +
       '</a>'
   };
