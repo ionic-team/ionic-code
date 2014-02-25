652753633e0ab7d39eac6e4a1432bde0a12923b2
Ionotron <hi@ionicframework.com>
2014-02-25 15:24:43 +0000

    release: 0.9.26-alpha-942 (nightly)

--- nightly/js/ionic.bundle.js
+++ nightly/js/ionic.bundle.js
@@ -8,7 +8,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v0.9.26-alpha-939
+ * Ionic, v0.9.26-alpha-942
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -23,7 +23,7 @@
 window.ionic = {
   controllers: {},
   views: {},
-  version: '0.9.26-alpha-939'
+  version: '0.9.26-alpha-942'
 };
 ;
 (function(ionic) {
@@ -31666,7 +31666,7 @@ angular.module('ui.router.compat')
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v0.9.26-alpha-939
+ * Ionic, v0.9.26-alpha-942
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
