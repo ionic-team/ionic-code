c51148bdbdf8af8fa15e1eefcdc7c997a00aac0f
Ionotron <hi@ionicframework.com>
2014-03-01 04:41:07 +0000

    release: 0.10.0-alpha-997 (nightly)

--- nightly/js/ionic.bundle.js
+++ nightly/js/ionic.bundle.js
@@ -8,7 +8,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v0.10.0-alpha-996
+ * Ionic, v0.10.0-alpha-997
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -24,7 +24,7 @@
 window.ionic = {
   controllers: {},
   views: {},
-  version: '0.10.0-alpha-996'
+  version: '0.10.0-alpha-997'
 };
 ;
 (function(ionic) {
@@ -31679,7 +31679,7 @@ angular.module('ui.router.compat')
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v0.10.0-alpha-996
+ * Ionic, v0.10.0-alpha-997
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
