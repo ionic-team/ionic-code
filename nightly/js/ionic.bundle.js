7b789a6e31021d3fe380160581fb891177121fa1
Ionotron <hi@ionicframework.com>
2014-03-20 20:03:24 +0000

    release: 0.9.27-nightly-1321 (nightly)

--- nightly/js/ionic.bundle.js
+++ nightly/js/ionic.bundle.js
@@ -8,7 +8,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v0.9.27-nightly-1320
+ * Ionic, v0.9.27-nightly-1321
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -24,7 +24,7 @@
 window.ionic = {
   controllers: {},
   views: {},
-  version: '0.9.27-nightly-1320'
+  version: '0.9.27-nightly-1321'
 };
 
 (function(ionic) {
@@ -32188,7 +32188,7 @@ angular.module('ui.router.compat')
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v0.9.27-nightly-1320
+ * Ionic, v0.9.27-nightly-1321
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
